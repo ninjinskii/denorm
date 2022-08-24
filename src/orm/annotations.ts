a62cc2f29b0afb4d98260a33abbda085271b05be
ninjinski <louis.bailly.pro@gmail.com>
2022-08-24 15:11:33 +0200

    Wip wip wip

--- src/orm/annotations.ts
+++ src/orm/annotations.ts
@@ -1,10 +1,10 @@
 // Ultra generic annotations, we expect weird type
-// deno-lint-ignore-file
+// deno-lint-ignore-file no-explicit-any
 
 import { Create, Field, SizeableType, Type } from "../query/create.ts";
 
-// deno-lint-ignore-file no-explicit-any
 let fields: Field[] = [];
+let resetOnNextInitTable = false;
 
 export enum Nullable {
   YES = "NULLABLE",
@@ -16,24 +16,22 @@ export interface Table {
   name: string;
 }
 
-export function initTable(_type: any, tableName: string) {
+export function initTable(_type: any, tableName: string): Field[] {
   // We wont use the type, but we need it to be evaluated.
   // Evaluating the type will trigger model's annotations
   // without us having to provide an instance of model
   // and describing fake parameters (e.g new Wine("", "", 1, ""))
   // to comply with TS type checks
 
+  console.log(`init table ${tableName}`);
+  const copy = [...fields];
   const create = new Create(
     { toDbField: (a) => a, fromDbField: (a) => a },
     tableName,
-    fields,
+    copy,
   );
 
-  console.log(`initTable ${tableName}:`);
-  console.log(fields);
-  console.log(create.toPreparedQuery().text);
-  // We might reset array here instead of in annotation functions
-  fields = [];
+  return copy;
 }
 
 export function Field(
@@ -43,80 +41,74 @@ export function Field(
 ) {
   return function (
     target: any,
-    propertyKey: string | symbol,
+    _propertyKey: string | symbol,
     parameterIndex: number,
   ) {
-    // maybeResetFieldArray(target, parameterIndex);
-
-    // Black magic to get actual class property name on which this annotation was placed
-    const name = Object.keys(new target())[parameterIndex];
-
-    // Note that last parameters annotation's runs first
-    fields.unshift({ type, primaryKey: false, as, nullable, name });
-
-    log(parameterIndex);
+    processAnnotation(target, parameterIndex, type, false, nullable, as);
   };
 }
 
 export function SizedField(
   type: Type,
-  size?: number,
+  size: number,
   nullable?: Nullable,
   as?: string,
 ) {
   return function (
     target: any,
-    propertyKey: string | symbol,
+    _propertyKey: string | symbol,
     parameterIndex: number,
   ) {
-    // maybeResetFieldArray(target, parameterIndex);
-
-    let s = size;
-
-    if (type! in SizeableType) {
-      s = undefined;
+    if (!(type in SizeableType)) {
+      throw new Error(
+        `The type ${type} cannot have a size. Use @Field() instead.`,
+      );
     }
 
-    // Black magic to get actual class property name on which this annotation was placed
-    const name = Object.keys(new target())[parameterIndex];
-
-    // Note that last parameters annotation's runs first
-    fields.unshift({ type, size: s, primaryKey: false, as, nullable, name });
-    log(parameterIndex);
+    processAnnotation(target, parameterIndex, type, false, nullable, as, size);
   };
 }
 
 export function PrimaryKey(type: Type, as?: string) {
   return function (
     target: any,
-    propertyKey: string | symbol,
+    _propertyKey: string | symbol,
     parameterIndex: number,
   ) {
-    // maybeResetFieldArray(target, parameterIndex);
-
-    // Black magic to get actual class property name on which this annotation was placed
-    const name = Object.keys(new target())[parameterIndex];
-
-    // Note that last parameters annotation's runs first
-    fields.unshift({
-      type,
-      primaryKey: true,
-      as,
-      nullable: Nullable.NO,
-      name,
-    });
-    log(parameterIndex);
+    processAnnotation(target, parameterIndex, type, true, Nullable.NO, as);
   };
 }
 
-function log(parameterIndex: number) {
-  console.log(`Processing annotation ${parameterIndex}`);
-}
+function processAnnotation(
+  target: any,
+  parameterIndex: number,
+  type: Type,
+  isPrimaryKey: boolean,
+  nullable?: Nullable,
+  as?: string,
+  size?: number,
+) {
+  console.log(`process annotation ${parameterIndex}`);
 
-function maybeResetFieldArray(target: any, parameterIndex: number) {
-  const fieldsCount = Object.keys(target).length;
-
-  if (parameterIndex === fieldsCount - 1) {
-    fields = Array(fieldsCount);
+  if (resetOnNextInitTable) {
+    fields = [];
+    resetOnNextInitTable = false;
   }
+
+  // Black magic to get actual class property name on which this annotation was placed
+  const keys = Object.keys(new target());
+  const name = keys[parameterIndex];
+  const length = keys.length;
+
+  // +1: paramaterIndex starts at 0
+  if (length === parameterIndex + 2) {
+    // We're on the last anotation processed (the first in the model object)
+    resetOnNextInitTable = true;
+  }
+
+  const primaryKey = isPrimaryKey || undefined;
+  const field = { type, primaryKey, as, nullable, name, size };
+
+  // Note that last parameters annotation's runs first
+  fields.unshift(field);
 }
