e89930fface1e799a719ead265b27ed9f043ac19
ninjinski <louis.bailly.pro@gmail.com>
2022-08-24 16:31:03 +0200

    Fix annotations test

--- tests/annotations.test.ts
+++ tests/annotations.test.ts
@@ -1,12 +1,13 @@
-import { type } from "https://deno.land/std@0.141.0/_wasm_crypto/crypto.wasm.mjs";
 import { Bottle } from "../fixture/model/bottle.ts";
 import { Wine } from "../fixture/model/wine.ts";
 import { initTables } from "../src/orm/annotations.ts";
 
+const databaseUrl = Deno.env.get("DATABASE_URL") || ""
+
 Deno.test("Create single table", () => {
-  initTables([Wine]);
+  initTables(databaseUrl, [Wine]);
 });
 
 Deno.test("Create multipe table", () => {
-  initTables([Wine, Bottle]);
+  initTables(databaseUrl, [Wine, Bottle]);
 });
