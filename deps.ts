21d3e6781c8b283931411617090411ab11faac6b
ninjinski <louis.bailly.pro@gmail.com>
2022-08-24 17:43:17 +0200

    Add test for annotations

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export { Client, Transaction } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.141.0/testing/asserts.ts";
+export { assertEquals, assertThrows } from "https://deno.land/std@0.141.0/testing/asserts.ts";
--- tests/annotations.test.ts
+++ tests/annotations.test.ts
@@ -1,13 +1,96 @@
-import { Bottle } from "../fixture/model/bottle.ts";
-import { Wine } from "../fixture/model/wine.ts";
-import { initTables } from "../src/orm/annotations.ts";
+import { assertEquals } from "../deps.ts";
+import {
+  Entity,
+  Field,
+  initTables,
+  Nullable,
+  PrimaryKey,
+} from "../src/orm/annotations.ts";
+import { FieldTransformer, QueryBuilder } from "../src/query/query-builder.ts";
+import { snakeCase } from "../src/util/case.ts";
 
-const databaseUrl = Deno.env.get("DATABASE_URL") || ""
+const databaseUrl = Deno.env.get("DATABASE_URL") || "";
+const transformer: FieldTransformer = {
+  toDbField: (clientField) => snakeCase(clientField),
+  fromDbField: (dbField) => dbField,
+  usePostgresNativeCamel: true,
+};
 
-Deno.test("Create single table", () => {
-  initTables(databaseUrl, [Wine]);
+const builder = new QueryBuilder(
+  transformer,
+  databaseUrl,
+);
+
+Deno.test("Create single table", async () => {
+  await withDatabase(async () => {
+    await dropTables();
+    await initTables(databaseUrl, [Annotations]);
+    await builder
+      .insert("annotations", [{ id: 1, comment: "Hi mom!" }])
+      .execute();
+
+    const result = await builder
+      .select("*")
+      .from("annotations")
+      .execute();
+
+    assertEquals(result, [{ id: 1, comment: "Hi mom!" }]);
+  });
 });
 
-Deno.test("Create multipe table", () => {
-  initTables(databaseUrl, [Wine, Bottle]);
+Deno.test("Create multipe table", async () => {
+  await withDatabase(async () => {
+    await dropTables();
+    await initTables(databaseUrl, [Annotations, Annotations2]);
+    await builder
+      .insert("annotations", [{ id: 2, comment: "Hi mom!" }])
+      .execute();
+
+    await builder
+      .insert("annotations_2", [{ id: 1, comment: null }])
+      .execute();
+
+    const result = await builder
+      .select("*")
+      .from("annotations")
+      .execute();
+
+    const result2 = await builder
+      .select("*")
+      .from("annotations_2")
+      .execute();
+
+    assertEquals(result, [{ id: 2, comment: "Hi mom!" }]);
+    assertEquals(result2, [{ id: 1, comment: null }]);
+  });
 });
+
+async function withDatabase(block: () => Promise<void>) {
+  await builder["executor"]["init"]();
+  await block();
+  await builder["executor"]["client"]?.end();
+}
+
+async function dropTables() {
+  await builder["executor"]["client"]?.queryObject(
+    "DROP TABLE IF EXISTS annotations, annotations_2;",
+  );
+}
+
+@Entity("annotations")
+class Annotations {
+  constructor(
+    @PrimaryKey("SERIAL") public id: number,
+    @Field("VARCHAR") public comment: string,
+  ) {
+  }
+}
+
+@Entity("annotations_2")
+class Annotations2 {
+  constructor(
+    @PrimaryKey("SERIAL") public id: number,
+    @Field("VARCHAR", Nullable.YES) public comment: string | null,
+  ) {
+  }
+}
