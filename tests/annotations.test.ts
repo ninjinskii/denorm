24bf4d8e2308ee8ce48f213996bcb5cb82a212fc
ninjinski <louis.bailly.pro@gmail.com>
2022-08-27 20:14:45 +0200

    Uncomment test

--- tests/annotations.test.ts
+++ tests/annotations.test.ts
@@ -1,121 +1,119 @@
-// DO NOT PUSH THIS UNCOMMENTED, IT WILL TRIGGER ANNOTATIONS ON CLIENT
+import { assertEquals } from "../deps.ts";
+import { initTables, SizedField } from "../src/orm/annotations.ts";
+import { QueryBuilder } from "../src/query/query-builder.ts";
 
-// import { assertEquals } from "../deps.ts";
-// import { initTables, SizedField } from "../src/orm/annotations.ts";
-// import { QueryBuilder } from "../src/query/query-builder.ts";
+const databaseUrl = Deno.env.get("DATABASE_URL") || "";
+const builder = new QueryBuilder(databaseUrl);
 
-// const databaseUrl = Deno.env.get("DATABASE_URL") || "";
-// const builder = new QueryBuilder(databaseUrl);
+Deno.test("Create single table", async () => {
+  await withDatabase(async () => {
+    await dropTables();
+    await initTables(databaseUrl, [Wine]);
+    await builder
+      .insert("wine", [{
+        id: 1,
+        naming: "Hi mom!",
+        name: null,
+        comment: "",
+        bottle_id: 1,
+        date: 1n,
+        taste_good: true,
+      }])
+      .execute();
 
-// Deno.test("Create single table", async () => {
-//   await withDatabase(async () => {
-//     await dropTables();
-//     await initTables(databaseUrl, [Wine]);
-//     await builder
-//       .insert("wine", [{
-//         id: 1,
-//         naming: "Hi mom!",
-//         name: null,
-//         comment: "",
-//         bottle_id: 1,
-//         date: 1n,
-//         taste_good: true,
-//       }])
-//       .execute();
+    const result = await builder
+      .select("*")
+      .from("wine")
+      .execute();
 
-//     const result = await builder
-//       .select("*")
-//       .from("wine")
-//       .execute();
+    assertEquals(result, [{
+      id: 1,
+      naming: "Hi mom!",
+      name: null,
+      comment: "",
+      bottleId: 1,
+      date: 1n,
+      tasteGood: true,
+    }]);
+  });
+});
 
-//     assertEquals(result, [{
-//       id: 1,
-//       naming: "Hi mom!",
-//       name: null,
-//       comment: "",
-//       bottleId: 1,
-//       date: 1n,
-//       tasteGood: true,
-//     }]);
-//   });
-// });
+Deno.test("Create multipe table", async () => {
+  await withDatabase(async () => {
+    await dropTables();
+    await initTables(databaseUrl, [Wine, Bottle]);
+    await builder
+      .insert("wine", [{
+        id: 1,
+        naming: "Hi mom!",
+        name: null,
+        comment: "",
+        bottle_id: 1,
+        date: 1n,
+        taste_good: true,
+      }])
+      .execute();
 
-// Deno.test("Create multipe table", async () => {
-//   await withDatabase(async () => {
-//     await dropTables();
-//     await initTables(databaseUrl, [Wine, Bottle]);
-//     await builder
-//       .insert("wine", [{
-//         id: 1,
-//         naming: "Hi mom!",
-//         name: null,
-//         comment: "",
-//         bottle_id: 1,
-//         date: 1n,
-//         taste_good: true,
-//       }])
-//       .execute();
+    await builder
+      .insert("bottle", [{ bottle_id: 1, bottle_size: "" }])
+      .execute();
 
-//     await builder
-//       .insert("bottle", [{ bottle_id: 1, bottle_size: "" }])
-//       .execute();
+    const result = await builder
+      .select("*")
+      .from("wine")
+      .execute();
 
-//     const result = await builder
-//       .select("*")
-//       .from("wine")
-//       .execute();
+    const result2 = await builder
+      .select("*")
+      .from("bottle")
+      .execute();
 
-//     const result2 = await builder
-//       .select("*")
-//       .from("bottle")
-//       .execute();
+    assertEquals(result2, [{ bottleId: 1, bottleSize: "" }]);
+    assertEquals(result, [{
+      id: 1,
+      naming: "Hi mom!",
+      name: null,
+      comment: "",
+      bottleId: 1,
+      date: 1n,
+      tasteGood: true,
+    }]);
+  });
+});
 
-//     assertEquals(result2, [{ bottleId: 1, bottleSize: "" }]);
-//     assertEquals(result, [{
-//       id: 1,
-//       naming: "Hi mom!",
-//       name: null,
-//       comment: "",
-//       bottleId: 1,
-//       date: 1n,
-//       tasteGood: true,
-//     }]);
-//   });
-// });
+async function withDatabase(block: () => Promise<void>) {
+  await builder["executor"]["init"]();
+  await block();
+  await builder["executor"]["client"]?.end();
+}
 
-// async function withDatabase(block: () => Promise<void>) {
-//   await builder["executor"]["init"]();
-//   await block();
-//   await builder["executor"]["client"]?.end();
-// }
+async function dropTables() {
+  await builder["executor"]["client"]?.queryObject(
+    "DROP TABLE IF EXISTS wine, bottle;",
+  );
+}
 
-// async function dropTables() {
-//   await builder["executor"]["client"]?.queryObject(
-//     "DROP TABLE IF EXISTS wine, bottle;",
-//   );
-// }
+import { Entity, Field, Nullable, PrimaryKey } from "../src/orm/annotations.ts";
 
-// import { Entity, Field, Nullable, PrimaryKey } from "../src/orm/annotations.ts";
+@Entity("bottle")
+class Bottle {
+  constructor(
+    @PrimaryKey("SERIAL", "bottle_id") public bottleId: number,
+    @Field("VARCHAR", Nullable.NO, "bottle_size") public bottleSize: string,
+  ) {
+  }
+}
 
-// @Entity("bottle")
-// class Bottle {
-//   constructor(
-//     @PrimaryKey("SERIAL", "bottle_id") public bottleId: number,
-//     @Field("VARCHAR", Nullable.NO, "bottle_size") public bottleSize: string,
-//   ) {
-//   }
-// }
-
-// @Entity("wine")
-// class Wine {
-//   constructor(
-//     @PrimaryKey("SERIAL") public id: number,
-//     @Field("VARCHAR") public naming: string,
-//     @Field("VARCHAR", Nullable.YES) public name: string | null,
-//     @SizedField("VARCHAR", 255) public comment: string,
-//     @Field("INT", Nullable.NO, "bottle_id") public bottleId: number,
-//     @Field("BIGINT") public date: bigint,
-//     @Field("BOOL", Nullable.NO, "taste_good") public tasteGood: boolean,
-//   ) {
-//   }
-// }
+@Entity("wine")
+class Wine {
+  constructor(
+    @PrimaryKey("SERIAL") public id: number,
+    @Field("VARCHAR") public naming: string,
+    @Field("VARCHAR", Nullable.YES) public name: string | null,
+    @SizedField("VARCHAR", 255) public comment: string,
+    @Field("INT", Nullable.NO, "bottle_id") public bottleId: number,
+    @Field("BIGINT") public date: bigint,
+    @Field("BOOL", Nullable.NO, "taste_good") public tasteGood: boolean,
+  ) {
+  }
+}
