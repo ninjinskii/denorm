c7c144e61fe84c71ba46b575e981aff5a7806e02
ninjinski <louis.bailly.pro@gmail.com>
2022-09-03 17:36:57 +0200

    Rework WHERE. It is now much simpler and for internal use only

--- src/query/where.ts
+++ src/query/where.ts
@@ -1,133 +1,47 @@
+// The value can be of whatever type
+// deno-lint-ignore-file no-explicit-any
+
 import { PreparedQueryText, QueryPart } from "./query.ts";
 
 export interface WhereCondition {
-  field: string;
-  equals?: number | string | boolean | null;
-  sup?: number;
-  inf?: number;
-  like?: string;
-}
-
-// Replace like, supe, inf with an enum named operator, & and & or by an enum named agregator
-export interface InternalWhereCondition {
-  field: string;
-  equals?: number | string | boolean | null;
-  sup?: number;
-  inf?: number;
-  like?: string;
-  and?: boolean;
+  [field: string]: any;
   or?: boolean;
-  chain?: boolean;
 }
 
 export class Where extends QueryPart {
-  private conditions: WhereCondition[] = [];
-  private combinedOnly = false;
-  private argsOffset = 0;
+  private conditions: WhereCondition;
+  private args: any[];
+  private prepared = false;
 
-  constructor(condition?: WhereCondition) {
+  constructor(conditions: WhereCondition, ...preparedArgs: any[]) {
     super();
+    this.conditions = conditions;
+    this.args = preparedArgs;
 
-    if (condition) {
-      this.conditions.push(condition);
-    } else {
-      this.combinedOnly = true;
+    if (preparedArgs.length > 0) {
+      this.prepared = true;
     }
-  }
-
-  setPreparedArgsOffset(offset: number) {
-    this.argsOffset = offset;
   }
 
   toText(): PreparedQueryText {
-    const args = [];
-    let text = "WHERE ";
-    let inChain = false;
-    let firstLoop = true;
-    let preparedArgsCounter = 1 + this.argsOffset;
+    const actualCondition = [];
+    let text = `WHERE `;
+    let preparedArgsIndex = 1;
 
-    for (const condition of this.conditions as InternalWhereCondition[]) {
-      let operator = "";
-      let value: unknown = "";
-      let agregator = "";
+    for (const [key, value] of Object.entries(this.conditions)) {
+      const val = this.prepared
+        ? `$${preparedArgsIndex++}`
+        : isNaN(value as any)
+        ? `'${value}'`
+        : value;
 
-      if (condition.equals !== undefined) {
-        operator = "=";
-        value = condition.equals;
-      } else if (condition.sup !== undefined) {
-        operator = ">";
-        value = condition.sup;
-      } else if (condition.inf !== undefined) {
-        operator = "<";
-        value = condition.inf;
-      } else if (condition.like !== undefined) {
-        operator = "LIKE";
-        value = condition.like;
-      }
-
-      const chainBeginning = inChain === false && condition.chain;
-      const chainEnd = inChain === true && !condition.chain;
-      const maybeOpenChain = chainBeginning ? "(" : "";
-      const maybeCloseChain = chainEnd ? ") " : "";
-
-      inChain = condition.chain || false;
-
-      if (condition.and && !(firstLoop && this.combinedOnly)) {
-        agregator = "AND";
-        text += " ";
-      } else if (condition.or) {
-        agregator = "OR";
-        text += " ";
-      } else {
-        agregator = "";
-      }
-
-      text +=
-        `${agregator} ${maybeOpenChain}${condition.field} ${operator} $${preparedArgsCounter++}${maybeCloseChain}`
-          .trim();
-
-      args.push(value);
-      firstLoop = false;
+      actualCondition.push(`${key} = ${val}`);
     }
 
-    return { text, args };
-  }
+    text += actualCondition.join(this.conditions.or ? " OR " : " AND ").concat(
+      ";",
+    );
 
-  private aggregate(
-    and: boolean,
-    condition: InternalWhereCondition | InternalWhereCondition[],
-  ) {
-    if (Array.isArray(condition)) {
-      if (condition.length === 0) {
-        return;
-      }
-
-      // Set chain to true on all conditions except last one
-      const last = condition.pop() as InternalWhereCondition;
-      last.and = and;
-      last.or = !and;
-
-      condition.forEach((cond) => {
-        cond.chain = true;
-        cond.and = and;
-        cond.or = !and;
-      });
-      condition.push(last);
-      this.conditions.push(...condition);
-    } else {
-      condition.and = and;
-      condition.or = !and;
-      this.conditions.push(condition);
-    }
-  }
-
-  and(condition: InternalWhereCondition | InternalWhereCondition[]) {
-    this.aggregate(true, condition);
-    return this;
-  }
-
-  or(condition: InternalWhereCondition | InternalWhereCondition[]) {
-    this.aggregate(false, condition);
-    return this;
+    return { text, args: this.args };
   }
 }
--- tests/query.test.ts
+++ tests/query.test.ts
@@ -23,86 +23,24 @@ Deno.test("Select multiple fields", () => {
 });
 
 Deno.test("Where single equals int", () => {
-  const conditions = { field: "wine_id", equals: 1 };
-  const { text, args } = new Where(conditions).toText();
+  const conditions = { wine_id: 1 };
+  const text = new Where(conditions).toText().text;
 
-  assertEquals(text, "WHERE wine_id = $1");
-  assertEquals(args, [1]);
+  assertEquals(text, "WHERE wine_id = 1;");
 });
 
 Deno.test("Where single equals string", () => {
-  const conditions = { field: "comment", equals: "Hi mom!" };
-  const { text, args } = new Where(conditions).toText();
+  const conditions = { comment: "Hi mom!" };
+  const  text = new Where(conditions).toText().text;
 
-  assertEquals(text, "WHERE comment = $1");
-  assertEquals(args, ["Hi mom!"]);
+  assertEquals(text, "WHERE comment = 'Hi mom!';");
 });
 
 Deno.test("Where two equals AND", () => {
-  const conditions = new Where({ field: "wine_id", equals: 1 })
-    .and({ field: "comment", equals: "Hi mom!" });
+  const conditions = new Where({ wine_id: 1, comment: "Hi mom!" })
+  const text = conditions.toText().text;
 
-  const { text, args } = conditions.toText();
-
-  assertEquals(text, "WHERE wine_id = $1 AND comment = $2");
-  assertEquals(args, [1, "Hi mom!"]);
-});
-
-Deno.test("Where multiple equals AND", () => {
-  const conditions = new Where({ field: "wine_id", equals: 1 })
-    .and({ field: "comment", equals: "Hi mom!" })
-    .and({ field: "type", equals: 1 });
-
-  const { text, args } = conditions.toText();
-
-  assertEquals(text, "WHERE wine_id = $1 AND comment = $2 AND type = $3");
-  assertEquals(args, [1, "Hi mom!", 1]);
-});
-
-Deno.test("Where multiple equals AND & OR", () => {
-  const conditions = new Where({ field: "wine_id", equals: 1 })
-    .and({ field: "comment", equals: "Hi mom!" })
-    .or({ field: "type", equals: 1 });
-
-  const { text, args } = conditions.toText();
-
-  assertEquals(text, "WHERE wine_id = $1 AND comment = $2 OR type = $3");
-  assertEquals(args, [1, "Hi mom!", 1]);
-});
-
-Deno.test("Where combined multiple equals AND", () => {
-  const conditions = new Where({ field: "wine_id", equals: 1 })
-    .and([{ field: "comment", equals: "Hi mom!" }, {
-      field: "type",
-      equals: 2,
-    }])
-    .or({ field: "type", equals: 1 });
-
-  const { text, args } = conditions.toText();
-  assertEquals(
-    text,
-    "WHERE wine_id = $1 AND (comment = $2 AND type = $3) OR type = $4",
-  );
-  assertEquals(args, [1, "Hi mom!", 2, 1]);
-});
-
-Deno.test("Where combined only multiple equals AND & OR", () => {
-  const conditions = new Where()
-    .and([
-      { field: "comment", equals: "Hi mom!" },
-      { field: "type", equals: 2 },
-    ])
-    .or([
-      { field: "type", equals: 1 },
-      { field: "type", equals: 3 },
-    ]);
-
-  const { text, args } = conditions.toText();
-  assertEquals(
-    text,
-    "WHERE (comment = $1 AND type = $2) OR (type = $3 OR type = $4)",
-  );
-  assertEquals(args, ["Hi mom!", 2, 1, 3]);
+  assertEquals(text, "WHERE wine_id = 1 AND comment = 'Hi mom!';");
 });
 
 Deno.test("Insert into", () => {
