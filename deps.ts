564f1edc9122e0c35f60755a5dc9bc6dc0465650
ninjinski <louis.bailly.pro@gmail.com>
2022-11-07 18:03:58 +0100

    Update postgres driver version

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Client, Transaction } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+export { Client, Transaction } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 export { assertEquals, assertThrows } from "https://deno.land/std@0.141.0/testing/asserts.ts";
